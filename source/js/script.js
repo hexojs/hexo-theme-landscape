5c9b26dc1f6f6ea342798fd852b9fb741765fa56
Tommy Chen <tommy351@gmail.com>
2013-12-22 21:06:11 +0800

    Modify header style: - Change banner picture - Add sub-navigation: RSS feed & search button

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,36 @@
 (function($){
+  // Search
+  var $searchWrap = $('#search-form-wrap'),
+    isSearchAnim = false,
+    searchAnimDuration = 400;
+
+  var startSearchAnim = function(){
+    isSearchAnim = true;
+  };
+
+  var stopSearchAnim = function(callback){
+    setTimeout(function(){
+      isSearchAnim = false;
+      callback && callback();
+    }, searchAnimDuration);
+  };
+
+  $('#nav-search-btn').on('click', function(){
+    if (isSearchAnim) return;
+
+    startSearchAnim();
+    $searchWrap.addClass('on');
+    stopSearchAnim(function(){
+      $('.search-form-input').focus();
+    });
+  });
+
+  $('.search-form-input').on('blur', function(){
+    startSearchAnim();
+    $searchWrap.removeClass('on');
+    stopSearchAnim();
+  });
+
   // Share
   $('body').on('click', function(){
     $('.article-share-box.on').removeClass('on');
