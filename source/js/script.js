32ac2a9bf65b475f1686f8d623a992fd796b6cac
Tommy Chen <tommy351@gmail.com>
2013-12-24 17:40:17 +0800

    Add fancybox tag

--- source/js/script.js
+++ source/js/script.js
@@ -92,13 +92,17 @@
 
   $('.article-entry').each(function(i){
     $(this).find('img').each(function(){
+      if (!$(this).parent().hasClass('fancybox')) return;
+
       var alt = this.alt;
 
-      if (alt){
-        $(this).after('<span class="caption">' + alt + '</span>');
-      }
+      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox" rel="gallery' + i + '"></a>')
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+    });
+
+    $(this).find('.fancybox').each(function(){
+      $(this).attr('rel', 'gallery' + i);
     });
   });
 
