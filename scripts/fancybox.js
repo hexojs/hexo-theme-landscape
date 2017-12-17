4a7b204b7a13886a590404dbfed8b3856bf488e7
MasuqaT <occar@hotmail.co.jp>
2017-12-18 08:21:07 +0900

    Extended fancybox tag.

--- scripts/fancybox.js
+++ scripts/fancybox.js
@@ -4,10 +4,22 @@ var rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:ww
  * Fancybox tag
  *
  * Syntax:
- *   {% fancybox /path/to/image [/path/to/thumbnail] [title] %}
+ *   {% fancybox [classes] /path/to/image [/path/to/thumbnail] [title] %}
  */
 
 hexo.extend.tag.register('fancybox', function (args) {
+    var classes = [];
+    const argc = args.length;
+    for (var i = 0; i < argc; i++) {
+        const arg = args.shift();
+        if (rUrl.test(arg) || arg[0] === '/' /* absolute path */) {
+            args.unshift(arg);
+            break;
+        }
+
+        // assume this is a HTML class
+        classes.push(arg);
+    }
     var original = args.shift(),
         thumbnail = '';
 
@@ -15,9 +27,10 @@ hexo.extend.tag.register('fancybox', function (args) {
         thumbnail = args.shift();
     }
 
+    const classesStr = classes.length > 0 ? ' ' + classes.join(' ') : '';
     var title = args.join(' ');
 
-    return '<div class="fancybox-tag">' +
+    return '<div class="fancybox-tag' + classesStr + '">' +
         '<a class="fancybox" href="' + original + '" title="' + title + '">' +
         '<img src="' + (thumbnail || original) + '" alt="' + title + '">' +
         '</a>' +
