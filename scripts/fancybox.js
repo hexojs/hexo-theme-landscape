8e5c6eaf57d9608c32c240e8d4a53055daa972e8
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-05 19:51:05 +0930

    fix: use fancybox@3 syntax

--- scripts/fancybox.js
+++ scripts/fancybox.js
@@ -17,8 +17,9 @@ hexo.extend.tag.register('fancybox', function(args){
 
   var title = args.join(' ');
 
-  return '<a class="fancybox" href="' + original + '" title="' + title + '">' +
+  return '<a data-fancybox="gallery" href="' + original + '" data-caption="' + title + '">' +
     '<img src="' + (thumbnail || original) + '" alt="' + title + '">'
     '</a>' +
     (title ? '<span class="caption">' + title + '</span>' : '');
 });
+
